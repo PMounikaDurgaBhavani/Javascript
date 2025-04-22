b9205b6c103e437b01a8fdce2df81ac721094434
PMounikaDurgaBhavani <pabbamounikadurgabhavani@gmail.com>
2025-04-22 12:21:06 +0530

    first commit

---
+++ JavaScript Fundamentals/variable.js
@@ -0,0 +1,30 @@
+//single line comments
+/*
+multiple 
+lines
+comments
+*/
+
+//variables ---> let, var, const
+
+//var---> it can be redeclared, updated
+//let---> it cannot be redeclared, can be updated
+//const---> it cannot be redeclared and cannot updated
+
+//we should not use reserved words as variable names
+
+
+console.log(num);
+//console.log(number);--> gives error
+let number;
+var num;
+console.log(number);
+console.log(num);
+number=10;
+console.log(number);
+number=100;
+console.log(number);
+num=2;
+console.log(num);
+num=5;
+console.log(num);
