469977f6182d4317411b2f1f32d235d213bf7e6a
PMounikaDurgaBhavani <pabbamounikadurgabhavani@gmail.com>
2025-04-22 12:33:11 +0530

    second commit

---
+++ JavaScript_Fundamentals/datatypes.js
@@ -0,0 +1,28 @@
+/* Datatypes
+Primitive
+   1. Number
+   2. BigInt
+   3. String
+   4. Boolean
+   5. Symbol
+   6. null
+   7. undefined
+Non-Primitive
+   8. Object   
+   */
+
+
+let num=100;
+console.log(typeof num);
+let a="Bhavani";
+console.log(typeof a);
+let b=false;
+console.log(typeof b);
+let c=null;
+console.log(c);
+let d;
+console.log(d);
+let e=Symbol("Hello Everyone, How are you?");
+console.log(e.description);
+let obj={name:"Mounika", age:"23"};
+console.log(typeof obj);
